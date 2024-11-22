0f9a7dcebf1c9722d6f52d485c099d5c87366a8c
Cabrini <cabrinirodriguez20@gmail.com>
2024-11-22 14:25:27 -0800

    edit

--- scripts/index.js
+++ scripts/index.js
@@ -15,6 +15,34 @@ document.addEventListener("DOMContentLoaded", () => {
   const cardContainer = document.querySelector(".cards__list");
   const cardTemplate = document.querySelector("#card-template");
 
+  // Declare initialCards array
+  const initialCards = [
+    {
+      name: "Yosemite Valley",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
+    },
+    {
+      name: "Lake Louise",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lake-louise.jpg",
+    },
+    {
+      name: "Bald Mountains",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/bald-mountains.jpg",
+    },
+    {
+      name: "Latemar",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/latemar.jpg",
+    },
+    {
+      name: "Vanoise National Park",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/vanoise.jpg",
+    },
+    {
+      name: "Lago di Braies",
+      link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/lago.jpg",
+    },
+  ];
+
   // --- Modal Functionality ---
   function openModal() {
     // Pre-fill the form fields with current profile data
